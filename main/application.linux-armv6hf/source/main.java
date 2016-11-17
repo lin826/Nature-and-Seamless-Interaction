dd1ca97ef1f8804850d0a5a467b39da97e9e4027
lin826 <liniju826@gmail.com>
2016-11-18 05:38:31 +0800

    Test CircleOn

--- main/application.linux-armv6hf/source/main.java
+++ main/application.linux-armv6hf/source/main.java
@@ -301,10 +301,10 @@ public void Swing(){
   m.alloff();
 }
 public void CircleOn(){
-  int[] p = {1,1,1};
+  int[] p = {8,7,6,5,4,3,2,1};
   for(int i=0;i<p.length;i++){
     setPorts(p);
-    m.switchon(ports,1,4000);
+    m.switchon(ports,3,4000);
     delay(200);
     for(int j=0;j<p.length;j++){
       p[j] = (p[j]+1)%p.length+1;
